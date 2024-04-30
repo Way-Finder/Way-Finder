4bbcf86910431fadd507a4680e6419bfd5c92fb5
David Magdy <divomagdy@gmail.com>
2024-04-30 17:32:38 +0300

    Finished The Rest of The Functionality
    Added the last utility functions for the class.
    - A helper function that helps determine if the trip cost is within a given budget.
    
    - A function that returns the trip combinations that satisfy the user's budget.
    
    - A function that prints the whole trip plan: Starting position, Destination, and budget.

--- Way-Finder/depthfirstsearch.cpp
+++ Way-Finder/depthfirstsearch.cpp
@@ -3,6 +3,7 @@
 #include <QMap>
 #include <QVector>
 #include <QPair>
+#include <QMultiMap>
 #include <QDebug>
 #include "adjgraph.h"
 
@@ -10,10 +11,11 @@
 This parameterized constructor sets the graph within the instance, saves the source and destination the user
 wants to travel from and to.
 */
-DepthFirstSearch::DepthFirstSearch(QString source,QString destination)
+DepthFirstSearch::DepthFirstSearch(QString source, QString destination, int budget)
 {
     this->source = source;
     this->destination = destination;
+    this->budget = budget;
 }
 
 /*
@@ -43,41 +45,88 @@ The algorithm consists of the following functionality:
 6. We unmark the previous node so we try a different combination.
 7. We pop our last transportation to try a different one.
 */
-void DepthFirstSearch::traverse(QString source, QVector<QPair<QString,Connection>> path, adjmap& graph)
+void DepthFirstSearch::traverse(QString source, QVector<QPair<QString, Connection>> path, adjmap& graph)
 {
-    this->visited[source] = 1;
-    for(auto it1 = graph[source].begin(); it1 != graph[source].end(); ++it1) // 1
+    this->mVisited[source] = 1; // Marking visited to prevent infinite loop
+    for(auto it1 = graph[source].begin(); it1 != graph[source].end(); ++it1) // # 1
     {
-        if(visited[it1.key()])
+        if(mVisited[it1.key()]) // Checking for visited again to prevent adding shared paths again.
         {
             continue;
         }
         else
         {
-            for(auto& transportation : it1.value()) // 2
+            for(auto& transportation : it1.value()) // # 2
             {
-                path.push_back(QPair<QString, Connection>(it1.key(), transportation));//3
-                if(isDestination(it1.key()))//4
+                path.push_back(QPair<QString, Connection>(it1.key(), transportation));// # 3
+                if(isDestination(it1.key()))// # 4
                 {
-                    this->pListOfPaths.append(path);
+                    this->mListOfPaths.append(path);
                     //return;
                 }
-                traverse(it1.key(),path,graph);//5
-                this->visited[it1.key()] = 0;//6
-                path.pop_back();//7
+                traverse(it1.key(),path,graph);// # 5
+                this->mVisited[it1.key()] = 0;// # 6
+                path.pop_back();// # 7
             }
         }
 
     }
 }
+
+
+/* This function calculates the net cost for the trip's
+transportations and return the cost if its valid, 0 otherwise.*/
+int DepthFirstSearch::isValidTrip(QVector<QPair<QString, Connection> > trip)
+{
+    int tripCost = 0;
+    for (auto& transportation: trip)
+    {
+        tripCost += transportation.second.cost;
+    }
+
+    if (tripCost <= this->budget)
+    {
+        return tripCost;
+    }
+
+    return 0;
+}
+
+
 // Public function for the user.
 void DepthFirstSearch::findMyWay(adjmap &graph)
 {
-    QVector<QPair<QString,Connection>> tempPath;
-    traverse(this->source,tempPath,graph);
+    QVector<QPair<QString, Connection>> tempPath;
+    traverse(this->source, tempPath, graph);
 }
-// Returning a full list of the lists of different combinations.
-QList<QVector<QPair<QString,Connection>>> DepthFirstSearch::getListOfPaths()
+
+
+// Returning a full list of different trips combination.
+QList<QVector<QPair<QString, Connection>>> DepthFirstSearch::getListOfPaths()
 {
-    return this->pListOfPaths;
+    return this->mListOfPaths;
+}
+
+
+// This function groups our valid trip in one multimap.
+QMultiMap<int, QVector<QPair<QString, Connection>>> DepthFirstSearch::getValidPaths()
+{
+    QMultiMap<int, QVector<QPair<QString, Connection>>> validTrips;
+    for (const QVector<QPair<QString, Connection>>& trip : mListOfPaths)
+    {
+        int currentTripCost = isValidTrip(trip);
+        if(currentTripCost)
+        {
+            validTrips.insert(currentTripCost,trip);
+        }
+    }
+
+    return validTrips;
+}
+
+
+// Returns the trips plan.
+QString DepthFirstSearch::getPlan()
+{
+    return "Current Location: " + this->source + " The Destination: " + this->destination + " The Budget is: " + QString::number(this->budget);
 }
--- Way-Finder/depthfirstsearch.h
+++ Way-Finder/depthfirstsearch.h
@@ -4,20 +4,24 @@
 #include <QMap>
 #include <QList>
 #include <QPair>
+#include <QMultiMap>
 #include "adjgraph.h"
 class DepthFirstSearch
 {
 private:
-    QList<QVector<QPair<QString,Connection>>> pListOfPaths;
-    QMap<QString,bool> visited;
+    QString source,destination;
+    int budget;
+    QList<QVector<QPair<QString,Connection>>> mListOfPaths;
+    QMap<QString,bool> mVisited;
     bool isDestination(QString current);
     void traverse(QString source, QVector<QPair<QString,Connection>> path,adjmap &graph);
+    int isValidTrip(QVector<QPair<QString,Connection>> trip);
 public:
-    QString source,destination;
-    DepthFirstSearch(QString source,QString destination);
+    DepthFirstSearch(QString source,QString destination,int budget);
     void findMyWay(adjmap &graph);
     QList<QVector<QPair<QString,Connection>>> getListOfPaths();
-
+    QMultiMap<int,QVector<QPair<QString,Connection>>> getValidPaths();
+    QString getPlan();
 };
 
 #endif // DEPTHFIRSTSEARCH_H
