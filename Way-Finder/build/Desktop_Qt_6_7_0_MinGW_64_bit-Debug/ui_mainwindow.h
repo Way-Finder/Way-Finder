f0fe8b32363b2bf4b5471f82db0f7bb2fd444f77
Parlerler <malekwa3er99@gmail.com>
2024-05-08 14:41:45 +0300

    Added some css styling

--- Way-Finder/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ Way-Finder/build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -27,10 +27,10 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QWidget *widget;
+    QWidget *layoutWidget;
     QVBoxLayout *verticalLayout_2;
     QGroupBox *groupBox;
-    QWidget *layoutWidget;
+    QWidget *layoutWidget1;
     QVBoxLayout *verticalLayout;
     QHBoxLayout *horizontalLayout;
     QLabel *label;
@@ -59,30 +59,37 @@ public:
         MainWindow->resize(947, 610);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
-        widget = new QWidget(centralwidget);
-        widget->setObjectName("widget");
-        widget->setGeometry(QRect(0, 10, 1011, 601));
-        verticalLayout_2 = new QVBoxLayout(widget);
+        layoutWidget = new QWidget(centralwidget);
+        layoutWidget->setObjectName("layoutWidget");
+        layoutWidget->setGeometry(QRect(0, 10, 1011, 601));
+        verticalLayout_2 = new QVBoxLayout(layoutWidget);
         verticalLayout_2->setObjectName("verticalLayout_2");
         verticalLayout_2->setContentsMargins(0, 0, 0, 0);
-        groupBox = new QGroupBox(widget);
+        groupBox = new QGroupBox(layoutWidget);
         groupBox->setObjectName("groupBox");
         groupBox->setStyleSheet(QString::fromUtf8("background-color: rgb(133, 138, 143);"));
-        layoutWidget = new QWidget(groupBox);
-        layoutWidget->setObjectName("layoutWidget");
-        layoutWidget->setGeometry(QRect(160, 60, 541, 221));
-        verticalLayout = new QVBoxLayout(layoutWidget);
+        layoutWidget1 = new QWidget(groupBox);
+        layoutWidget1->setObjectName("layoutWidget1");
+        layoutWidget1->setGeometry(QRect(160, 60, 541, 221));
+        verticalLayout = new QVBoxLayout(layoutWidget1);
         verticalLayout->setObjectName("verticalLayout");
         verticalLayout->setContentsMargins(0, 0, 0, 0);
         horizontalLayout = new QHBoxLayout();
         horizontalLayout->setObjectName("horizontalLayout");
-        label = new QLabel(layoutWidget);
+        label = new QLabel(layoutWidget1);
         label->setObjectName("label");
+        label->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);"));
 
         horizontalLayout->addWidget(label);
 
-        depcity = new QComboBox(layoutWidget);
+        depcity = new QComboBox(layoutWidget1);
         depcity->setObjectName("depcity");
+        depcity->setStyleSheet(QString::fromUtf8("\n"
+"  color: black;\n"
+"  background-color: white;\n"
+"  border: 3px solid black;\n"
+"  padding: 2px;\n"
+""));
 
         horizontalLayout->addWidget(depcity);
 
@@ -91,13 +98,20 @@ public:
 
         horizontalLayout_2 = new QHBoxLayout();
         horizontalLayout_2->setObjectName("horizontalLayout_2");
-        label_2 = new QLabel(layoutWidget);
+        label_2 = new QLabel(layoutWidget1);
         label_2->setObjectName("label_2");
+        label_2->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);"));
 
         horizontalLayout_2->addWidget(label_2);
 
-        destcity = new QComboBox(layoutWidget);
+        destcity = new QComboBox(layoutWidget1);
         destcity->setObjectName("destcity");
+        destcity->setStyleSheet(QString::fromUtf8("\n"
+"  color: black;\n"
+"  background-color: white;\n"
+"  border: 3px solid black;\n"
+"  padding: 2px;\n"
+""));
 
         horizontalLayout_2->addWidget(destcity);
 
@@ -106,19 +120,26 @@ public:
 
         horizontalLayout_3 = new QHBoxLayout();
         horizontalLayout_3->setObjectName("horizontalLayout_3");
-        label_3 = new QLabel(layoutWidget);
+        label_3 = new QLabel(layoutWidget1);
         label_3->setObjectName("label_3");
+        label_3->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);"));
 
         horizontalLayout_3->addWidget(label_3);
 
-        label_4 = new QLabel(layoutWidget);
+        label_4 = new QLabel(layoutWidget1);
         label_4->setObjectName("label_4");
+        label_4->setMinimumSize(QSize(152, 0));
 
         horizontalLayout_3->addWidget(label_4);
 
-        pricein = new QLineEdit(layoutWidget);
+        pricein = new QLineEdit(layoutWidget1);
         pricein->setObjectName("pricein");
-        pricein->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        pricein->setStyleSheet(QString::fromUtf8("\n"
+"  color: black;\n"
+"  background-color: white;\n"
+"  border: 3px solid black;\n"
+"  padding: 2px;\n"
+""));
 
         horizontalLayout_3->addWidget(pricein);
 
@@ -127,7 +148,12 @@ public:
 
         label_5 = new QLabel(groupBox);
         label_5->setObjectName("label_5");
-        label_5->setGeometry(QRect(320, 10, 241, 41));
+        label_5->setGeometry(QRect(390, 10, 151, 41));
+        QFont font;
+        font.setPointSize(16);
+        font.setUnderline(true);
+        label_5->setFont(font);
+        label_5->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);"));
         groupBox_2 = new QGroupBox(groupBox);
         groupBox_2->setObjectName("groupBox_2");
         groupBox_2->setGeometry(QRect(-10, 300, 961, 301));
@@ -136,22 +162,53 @@ public:
         BEFES = new QPushButton(groupBox_2);
         BEFES->setObjectName("BEFES");
         BEFES->setGeometry(QRect(440, 180, 121, 51));
+        BEFES->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"  background-color: rgb(133, 138, 143);\n"
+"  color: black;\n"
+"  border: 3px solid black;\n"
+"  border-radius: 20px;\n"
+"  padding: 5px 10px;\n"
+"  cursor: pointer;\n"
+"  transition: background-color 0.2s ease;\n"
+"}\n"
+"\n"
+"QPushButton :hover {\n"
+"  background-color: rgb(102, 107, 112);\n"
+"}\n"
+"\n"
+"QPushButton: active {\n"
+"  background-color: rgb(71, 77, 82);\n"
+"}"));
         DEFES = new QPushButton(groupBox_2);
         DEFES->setObjectName("DEFES");
         DEFES->setGeometry(QRect(440, 60, 121, 51));
+        DEFES->setStyleSheet(QString::fromUtf8("background-color: rgb(133, 138, 143);;\n"
+"color: black;\n"
+"border: 3px solid black;\n"
+"border-radius: 20px;\n"
+"padding: 5px 10px; \n"
+""));
         pushButton = new QPushButton(groupBox_2);
         pushButton->setObjectName("pushButton");
         pushButton->setGeometry(QRect(810, 230, 121, 41));
+        pushButton->setStyleSheet(QString::fromUtf8("background-color: rgb(133, 138, 143);;\n"
+"color: black;\n"
+"border: 3px solid black;\n"
+"border-radius: 20px;\n"
+"padding: 5px 10px; \n"
+""));
         label_6 = new QLabel(groupBox_2);
         label_6->setObjectName("label_6");
         label_6->setGeometry(QRect(280, 180, 101, 51));
-        QFont font;
-        font.setPointSize(12);
-        label_6->setFont(font);
+        QFont font1;
+        font1.setPointSize(12);
+        label_6->setFont(font1);
+        label_6->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);"));
         label_7 = new QLabel(groupBox_2);
         label_7->setObjectName("label_7");
         label_7->setGeometry(QRect(280, 60, 101, 51));
-        label_7->setFont(font);
+        label_7->setFont(font1);
+        label_7->setStyleSheet(QString::fromUtf8("color: rgb(0, 0, 0);"));
         DFS_pic = new QLabel(groupBox_2);
         DFS_pic->setObjectName("DFS_pic");
         DFS_pic->setGeometry(QRect(610, 20, 151, 121));
@@ -176,7 +233,7 @@ public:
         label_2->setText(QCoreApplication::translate("MainWindow", "Arrival City", nullptr));
         label_3->setText(QCoreApplication::translate("MainWindow", "Enter your Budget", nullptr));
         label_4->setText(QString());
-        label_5->setText(QCoreApplication::translate("MainWindow", "WLECOME TO WAYFINDER", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "WAYFINDER", nullptr));
         groupBox_2->setTitle(QString());
         BEFES->setText(QCoreApplication::translate("MainWindow", "BFS", nullptr));
         DEFES->setText(QCoreApplication::translate("MainWindow", "DFS", nullptr));
